ac03fb97951a802058999e3fa84eda304751db69
FatimaZelay <fatima.elay@gmail.com>
2025-09-18 20:49:06 +0200

    fix: juiste data ophalen voor de detailpagina 2E #4

--- squadpage/src/routes/squad2e/+page.server.js
+++ squadpage/src/routes/squad2e/+page.server.js
@@ -1,6 +1,10 @@
-export async function load({ url}) {
-    const membersResponse = await fetch ("https://fdnd.directus.app/items/person?fields=avatar,name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&filter[squads][squad_id][name][_eq]=2E&sort=name");
-    const membersData = await membersResponse.json();
-
-    return {members: membersData. data};
-}
+export async function load({ fetch }) {
+    const url =
+      "https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202&filter[squads][squad_id][name][_eq]=2E&sort=name";
+  
+    const student = await fetch(url);
+    const cards = await student.json();
+  
+    return { members: cards.data };
+  }
+  
